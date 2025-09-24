b416e0f756c114f43658dd043c470d5df11fd9d2
Dmitrijs Vetrovs <a242240dv@rvt.lv>
2025-09-24 09:59:13 +0300

    Detaļas

---
+++ src/main/java/rvt/discountBolts.java
@@ -0,0 +1,35 @@
+package rvt;
+import java.util.Scanner;
+
+public class discountBolts {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Skrūvju skaits: ");
+        int screw = Integer.valueOf(input.nextLine());
+        final int screwPrice = 5;
+
+        System.out.print("Uzgriežņu skaits: ");
+        int nut = Integer.valueOf(input.nextLine());
+        final int nutPrice = 3;
+
+        System.out.print("Paplāksņu skaits: ");
+        int washer = Integer.valueOf(input.nextLine());
+        final int washerPrice = 1;
+
+        
+        System.out.println();
+        if(screw>nut){
+            System.out.println("Parbaudi pasūtījumu: par maz uzgriežņu");
+        } 
+        if(screw*2>washer){
+            System.out.println("Parbaudi pasūtījumu: par maz paplāksņu");
+        } else{
+            System.out.println("Pasūtījums ir kārtībā.");
+        }
+
+        System.out.println();
+        int finalPrice = screw*screwPrice + washer*washerPrice + nut*nutPrice;
+        System.out.println("Kopējā cena: " + finalPrice);
+    }
+}
